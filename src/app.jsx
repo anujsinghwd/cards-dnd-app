2ba497246c97d5e39a1b0f271112bd7ee24ce438
anujsinghwd <anuj_singh@outlook.in>
2021-08-28 15:17:32 +0530

    Initial App Setup With Webpack

---
+++ src/app.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return 'Hey User!';
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: "./src/index.js",
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.html$/i,
+                use: [
+                    {
+                        loader: 'html-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader', {
+                        loader: 'css-loader', options: {
+                            modules: {
+                                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                            },
+                        }
+                    },
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.scss', '.json']
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        historyApiFallback: true
+    }
+}
